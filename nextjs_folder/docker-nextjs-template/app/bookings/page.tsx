9d031ea36779946893f4a48d9f4fc475e487144f
menaceNez <stankgav@gmail.com>
2025-10-02 11:07:00 -0500

    small snippet in /bookings for where to get an active users session

--- nextjs_folder/docker-nextjs-template/app/bookings/page.tsx
+++ nextjs_folder/docker-nextjs-template/app/bookings/page.tsx
@@ -8,11 +8,18 @@ import { User } from "next-auth";
  *  operations to the server with api calls
  */
 
+/**
+ * Accessing the session:
+ *  if "use server" => use: const session = await auth();
+ *  if "use client" => use   const { data: session, status } = useSession();
+ *    For the useSession() version: This is a react hook, this hook will call backend to get the users session
+ */
+
 // export: allow the current page, allowing other files to import this file.
 // default: this is what tells nextjs which react component to show as the root component on this route (/demo)
 export default async function Page() {
-    //const user;
-    
+  //const user;
+
   return (
     <div className="flex justify-center items-center flex-col">
       <p>This is the appointments page for my jira ticket</p>
