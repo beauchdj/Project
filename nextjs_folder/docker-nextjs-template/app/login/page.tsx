9b168c1e86d379b03b507f22a2afeb3a5c5ce784
beauchdj <beauchdj@gmail.com>
2025-10-05 08:39:47 -0500

    Hey things are looking up!

--- nextjs_folder/docker-nextjs-template/app/login/page.tsx
+++ nextjs_folder/docker-nextjs-template/app/login/page.tsx
@@ -29,62 +29,64 @@ export default function Page() {
   }
 
   return (
-    <main>
-      {/* <Image className="" src={myImage} /> */}
-       {/* <div className="min-h-screen bg-[url('/ph1.jpg')] bg-cover bg-center flex items-center justify-center p-6"> */}
-      <div className="flex h-screen items-center justify-center space-x-10">
-        <div className="flex w-1/4 h-5/8 rounded-2xl bg-emerald-900 relative size-90">
-          <form onSubmit={loginHandler} className="">
-            <label htmlFor="username" className="">
-              Username
-            </label>
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              className="input-element"
-              required
-            />
-            <label htmlFor="password" className="">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              className="input-element"
-              required
-            />
+    <div className="min-h-screen bg-[url('/ph1.jpg')] bg-cover bg-center flex items-center justify-center p-6">
 
-            {isError && (
-              <p className="text-red-500 w-9/10 text-center font-bold">
-                Invalid Credentials
-              </p>
-            )}
-            <button className="btn shadow shadow-black mt-1" type="submit">
-              Login
-            </button>
-            <br></br>
-            <br></br>
-            <div className="flex items-center justify-center">
-              <h3 className="text-center">Welcome back! Lets get you Schwell!</h3>
-            </div>
-          </form>
-
-
-        </div>
-        <div className="flex items-center justify-center bg-emerald-900 w-1/4 h-5/8 rounded-2xl relative size-90 ">
-          <p className="text-center">
-            Don&apos;t have an account?{" "}
-            <a
-              href={"/register"}
-              className="underline text-blue-600 visited:text-purple-500 hover:text-blue-200"
-            >
-              Register
-            </a>
-          </p>
-        </div>
+      <main>
+              <div className="flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-extrabold italic font-italic opacity-70 text-slate-900 text-8xl md:text-6xl">
+          Welcome to Schwellness!
+        </h1>
       </div>
-    </main>
+        <div className="flex h-screen items-center justify-center space-x-10 opacity-84">
+          <div className="flex items-center justify-center bg-emerald-900 w-3/8 h-4/8 rounded-2xl relative size- backdrop-filter backdrop-blur-lg">
+            <form onSubmit={loginHandler} className="text-center">
+              <label htmlFor="username" className="text-center">
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                className="input-element text-center"
+                required
+              />
+              <label htmlFor="password" className="text-center">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="input-element text-center"
+                required
+              />
+
+              {isError && (
+                <p className="text-red-500 text-center font-bold">
+                  Invalid Credentials
+                </p>
+              )}
+              <button className="btn shadow shadow-black mt-1" type="submit">
+                Login
+              </button>
+            </form>
+
+
+          </div>
+          <div className="flex items-center justify-center bg-emerald-900 w-3/8 h-4/8 rounded-2xl relative size-90">
+            <p className="text-center">
+              Don&apos;t have an account?{" "}
+              <br></br>
+              <a
+                href={"/register"}
+                className="underline text-blue-600 visited:text-purple-500 hover:text-blue-200 text-2xl"
+              >
+                Register
+              </a>
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
