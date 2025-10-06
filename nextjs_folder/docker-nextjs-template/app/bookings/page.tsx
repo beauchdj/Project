5fed8059eaab862c6c4c0540a714adf3036058c3
menaceNez <stankgav@gmail.com>
2025-10-06 13:46:41 -0500

    worked on bookings page

--- nextjs_folder/docker-nextjs-template/app/bookings/page.tsx
+++ nextjs_folder/docker-nextjs-template/app/bookings/page.tsx
@@ -1,13 +1,63 @@
 "use server";
-import { auth } from "../../auth"
+import { pool } from "@/lib/db";
+import { auth } from "../../auth";
 import { redirect } from "next/navigation";
+import { CalendarComponent } from "../lib/components/calendar";
+
+type booking = {
+  id: string;
+  spid: string;
+  starttime: Date;
+  endtime: Date;
+  service: string;
+};
 
 export default async function Page() {
-    const session = await auth()
-    if (!session) redirect("/login");
+  const session = await auth();
+  if (!session) redirect("/login");
+  const bookings: booking[] = (await getBookings()) as booking[];
+
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    timeZone: "America/Chicago",
+    month: "long",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  const first = bookings?.at(0);
+  const start = formatter.format(first?.starttime);
+  const end = formatter.format(first?.endtime);
   return (
     <div>
       <h1>This is the Bookings Page for {session.user.fullname}</h1>
+      <div className="flex bg-emerald-700 h-[40vh] flex-col">
+        <CalendarComponent />
+        {bookings?.map((booking: booking, idx) => (
+          <div
+            key={booking.id}
+            className="grid grid-cols-3 text-xs w-full h-fit hover:border-white hover:border-1 m-1"
+          >
+            <div className="text-center">{booking.service}</div>
+            <div className="text-center">{start}</div>
+            <div className="text-center">{end}</div>
+            {formatter.format(booking.starttime)}
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
+}
+
+async function getBookings() {
+  try {
+    const res = await pool.query("SELECT * FROM appts_avail");
+
+    console.log("Got bookings: ", res);
+    return res.rows;
+  } catch (error) {
+    console.log("ERROR getBookings(): ", error);
+    return null;
+  }
 }
--- nextjs_folder/docker-nextjs-template/app/lib/components/dropdown.tsx
+++ nextjs_folder/docker-nextjs-template/app/lib/components/dropdown.tsx
@@ -8,45 +8,51 @@ import Link from "next/link";
 export default function Dropdown({ session }: { session?: Session | null }) {
   // const [selected, setSelected] = useState('');
   let isSp = false;
+  let isCust = false;
   if (session && session.user) {
     isSp = session.user.isSp!;
+    isCust = session.user.isCustomer!;
+
     console.log("should set isSp: ", isSp, session.user);
   }
   return (
-    <main className="">
-      <div className="group relative p-1 cursor-default">
-        <div className="flex flex-col gap-0.5">
-          <div className="hover-bar" />
-          <div className="hover-bar" />
-          <div className="hover-bar" />
-          <div
-            className={
-              "group-hover:flex group-hover:absolute flex-col hidden top-0 left-[-2em] z-10 bg-emerald-900 w-fit h-fit rounded-xl" // default behavior is hidden, when the group is hovered it will stay with group-hover:...
-            }
-          >
-            <Link className="nav-btn" href={"/"}>
-              Root
+    <div className="group relative p-1 cursor-default">
+      <div className="flex flex-col gap-0.5">
+        <div className="hover-bar" />
+        <div className="hover-bar" />
+        <div className="hover-bar" />
+        <div
+          className={
+            "group-hover:flex group-hover:absolute flex-col hidden top-0 left-[-2em] z-10 bg-emerald-900 w-fit h-fit rounded-xl" // default behavior is hidden, when the group is hovered it will stay with group-hover:...
+          }
+        >
+          <Link className="nav-btn" href={"/"}>
+            Root
+          </Link>
+          <Link className="nav-btn" href={"/home"}>
+            Home
+          </Link>
+          <Link className="nav-btn" href={"/login"}>
+            Login
+          </Link>
+          <Link href={"/demo"} className={"nav-btn"}>
+            Demo
+          </Link>
+          <Link href={"/register"} className={"nav-btn"}>
+            Register
+          </Link>
+          {/* {isSp && ( */}
+          <Link href={"/appointments"} className={"nav-btn text-xs"}>
+            Edit Availability
+          </Link>
+          {/* )} */}
+          {isCust && (
+            <Link href={"/bookings"} className="nav-btn">
+              Bookings
             </Link>
-            <Link className="nav-btn" href={"/home"}>
-              Home
-            </Link>
-            <Link className="nav-btn" href={"/login"}>
-              Login
-            </Link>
-            <Link href={"/demo"} className={"nav-btn"}>
-              Demo
-            </Link>
-            <Link href={"/register"} className={"nav-btn"}>
-              Register
-            </Link>
-            {/* {isSp && ( */}
-            <Link href={"/appointments"} className={"nav-btn"}>
-              Create Appt Availability
-            </Link>
-            {/* )} */}
-          </div>
+          )}
         </div>
       </div>
-    </main>
+    </div>
   );
 }
