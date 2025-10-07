63b83b9ebede21eedf8eece27817ed2602ebe861
Jaclyn Sobolewski <124474391+JQuelll@users.noreply.github.com>
2025-10-07 11:50:09 -0500

    added searchAppts component

--- nextjs_folder/docker-nextjs-template/app/bookings/page.tsx
+++ nextjs_folder/docker-nextjs-template/app/bookings/page.tsx
@@ -6,7 +6,8 @@ import SearchAppts from "./SearchAppts";
 export default async function Page() {
     const session = await auth()
     if (!session) redirect("/login");
-  return (
+  
+    return (
      <main className="w-full bg-emerald-900 text-white px-4 py-3">
       <h1 className="text-lg font-semibold mb-3">Find and Book Appointments</h1>
       <SearchAppts />
