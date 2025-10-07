a4cb0aa06790b60b32b6459bc2d5297670b54ef3
Jaclyn Sobolewski <124474391+JQuelll@users.noreply.github.com>
2025-10-06 22:08:19 -0500

    added apptsList component

--- nextjs_folder/docker-nextjs-template/app/appointments/page.tsx
+++ nextjs_folder/docker-nextjs-template/app/appointments/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "../../auth"
 import { redirect } from "next/navigation";
 import CreateApptForm from "./CreateApptForm";
+import AppointmentsList from "./AppointmentsList";
 
 export default async function CreateAppointmentsPage() {
   const session = await auth()
@@ -8,13 +9,13 @@ export default async function CreateAppointmentsPage() {
   if (!session.user.isSp) redirect("/");
   
   return (
-     <main className="max-w-full px-4 py-2">
-        <div className="sticky top-0 z-10 bg-emerald-900/90 backdrop-blur-sm p-2">
-          <CreateApptForm />
-        </div>
-        <div>
-          Upcoming Appointments List (coming soon...)
-        </div>
-      </main>
+    <main className="max-w-full px-4 py-2">
+      <div className="sticky top-0 z-10 bg-emerald-900/90 backdrop-blur-sm p-2">
+        <CreateApptForm />
+      </div>
+      <div className="my-4 font-semibold text-lg">
+        <AppointmentsList />
+      </div>
+    </main>
   )
 }
