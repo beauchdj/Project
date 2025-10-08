c29f20bfc407e41e7007eb60c8bfa192089f798a
Jaclyn Sobolewski <124474391+JQuelll@users.noreply.github.com>
2025-10-08 10:18:35 -0500

    added TODO list

--- nextjs_folder/docker-nextjs-template/app/bookings/page.tsx
+++ nextjs_folder/docker-nextjs-template/app/bookings/page.tsx
@@ -1,3 +1,14 @@
+//TODO:
+// add other search filters to look for appointments
+// make category search filter a dropdown
+// add date column
+// pretty format times
+// check for conflicts when booking
+// auto-refresh available appointments and good messaging when booked (or not)
+// decide where the list of booked appointments should live
+// error handling, loading state, redirection
+
+
 "use server";
 import { auth } from "../../auth"
 import { redirect } from "next/navigation";
