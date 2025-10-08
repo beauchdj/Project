8965f870432b363d9e14f9252825e126b9bac74d
Jaclyn Sobolewski <124474391+JQuelll@users.noreply.github.com>
2025-10-08 10:17:32 -0500

    added TODO list

--- nextjs_folder/docker-nextjs-template/app/appointments/page.tsx
+++ nextjs_folder/docker-nextjs-template/app/appointments/page.tsx
@@ -1,3 +1,12 @@
+//TODO:
+// add date column
+// pretty format times
+// add button to delete available appt (and create api endpoints)
+// sort/filter/paginate appointment list
+// check for conflicts when creating appointments
+// error handling, loading state, redirection
+
+
 import { auth } from "../../auth"
 import { redirect } from "next/navigation";
 import CreateApptForm from "./CreateApptForm";
