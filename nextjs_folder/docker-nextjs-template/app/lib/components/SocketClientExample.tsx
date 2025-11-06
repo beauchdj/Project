9dd255011ab19861118cf78ccf1b603746407635
menaceNez <stankgav@gmail.com>
2025-11-05 22:07:17 -0600

    Added websocket support for the notification system, this comes with the wss page that allows you to register a username and send messages trace the code to learn the basic set up. It uses the Socket.io library.

---
+++ nextjs_folder/docker-nextjs-template/app/lib/components/SocketClientExample.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { FormEvent, useEffect, useState } from "react";
+import { socket } from "../services/websocket";
+
+export function WebSocketClient() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
+  const [messages, setMessages] = useState<string[]>([]);
+  const [send, setSend] = useState("");
+  const [username, setUsername] = useState("");
+  const [notify, setNotify] = useState(false);
+  const [hasUsername, setHasUsername] = useState(false);
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+      console.log(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        // transport defines what type of protocol we are using to send messages, http polling or websockets
+        // http polling is when a request is sent and held until a response is available, not blocking because other requests are unique messages
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      console.log("Disconnecting...");
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    // socket.on("message", (resp) => {
+    //   console.log("HIT MESSAGE: ", resp);
+    // });
+
+    socket.on("response", (resp) => {
+      console.log("response: ", resp);
+      setMessages((prev) => [...prev, resp]);
+    });
+
+    socket.on("notify", (response) => {
+      console.log("notify: ", response);
+      setNotify((prev) => !prev);
+    });
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
+
+  function handleMessage(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const res = new FormData(e.currentTarget);
+    const message = res.get("message");
+
+    socket.emit("chat message", message);
+    setSend(""); // reset input
+  }
+  function handleUsername(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const res = new FormData(e.currentTarget);
+    const username = res.get("username") as string;
+
+    socket.emit("set username", username);
+    setUsername(username);
+    setHasUsername(true);
+  }
+
+  return (
+    <div className="flex justify-center items-center flex-col text-white">
+      <div
+        className="bg-blue-400 h-18 w-18 flex items-center justify-center "
+        hidden={notify}
+      >
+        Follow!
+      </div>
+      <p>status: {isConnected ? "connected" : "disconnected"}</p>
+      <p>transport: {transport} </p>
+      <div className="bg-orange-300 h-100 w-100 p-4 rounded">
+        <div className="bg-pink-100 h-60 w-92 rounded p-1 text-black font-bold overflow-auto">
+          {messages.map((str, idx) => (
+            <div key={idx}>{str}</div>
+          ))}
+        </div>
+        <form onSubmit={handleMessage}>
+          <label className="text-black flex flex-col">
+            Send a message:
+            <input
+              type="text"
+              name="message"
+              value={send || ""}
+              onChange={(e) => setSend(e.target.value)}
+              placeholder="Send a message"
+              className="bg-gray-300 rounded text-black border-black border indent-1 mt-1"
+            ></input>
+          </label>
+        </form>
+        <form onSubmit={handleUsername}>
+          <label className={"text-black flex flex-col"}>
+            Set a username:
+            <input
+              type="text"
+              name="username"
+              value={username || ""}
+              placeholder="Set username"
+              className="bg-gray-300 rounded text-black border-black border indent-1 mt-1"
+              disabled={hasUsername}
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+          </label>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ nextjs_folder/docker-nextjs-template/app/wss/page.tsx
@@ -0,0 +1,9 @@
+import { WebSocketClient } from "../lib/components/SocketClientExample";
+
+export default function Wss() {
+  return (
+    <div>
+      <WebSocketClient />
+    </div>
+  );
+}
