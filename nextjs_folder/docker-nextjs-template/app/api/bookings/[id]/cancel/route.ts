0a56c83c4657db5b19d397a1cc05616bc4af33f6
Jaclyn Sobolewski <124474391+JQuelll@users.noreply.github.com>
2025-11-10 15:03:52 -0600

    fix return codes if error

--- nextjs_folder/docker-nextjs-template/app/api/bookings/[id]/cancel/route.ts
+++ nextjs_folder/docker-nextjs-template/app/api/bookings/[id]/cancel/route.ts
@@ -7,14 +7,18 @@ export async function PUT(request : Request, { params }: { params: { id: string 
         if (!session) {
             return NextResponse.json({error: "Unauthorized" }, { status: 401 });
         }
-    const bookingId = params.id
+    const bookingId = await params.id;
     if (!bookingId) {
         return NextResponse.json({ error: "Missing bookingId" }, { status: 400 });
     }
 
     try {
         const result = await cancelBookedAppt(bookingId, session.user.id);
-        return NextResponse.json(result, {status: 200 });
+        if(result.ok) {
+            return NextResponse.json(result, {status: 200 });
+        } else {
+            return NextResponse.json(result, {status: 403});
+        }
     } catch (error) {
         return NextResponse.json({ error: "Server Error" }, { status: 500 });
     }
