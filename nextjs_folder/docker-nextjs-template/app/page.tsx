c4d11d5f3a6e49f71dbfb83e856abe325a050a71
beauchdj <beauchdj@gmail.com>
2025-11-24 18:59:51 -0600

    random updates here and there

--- nextjs_folder/docker-nextjs-template/app/page.tsx
+++ nextjs_folder/docker-nextjs-template/app/page.tsx
@@ -15,8 +15,8 @@ export default async function page() {
 
   return (
     <main className="w-full flex items-center justify-center text-lg flex-col overflow-auto">
-      <div className="flex flex-col items-center w-[95%] bg-black/70 rounded-xl py-4 h-[87vh] shadow-black shadow-xl mt-2">
-        <div className="w-full flex flex-col items-center gap-3 h-[75vh] overflow-auto">
+      <div className="flex flex-col items-center w-[95%] bg-black/70 rounded-xl py-4 h-[87vh] shadow-black shadow-xl mt-2 z-20">
+        <div className="w-full flex flex-col items-center gap-3 h-[75vh]">
           <span className="flex-none text-4xl sm:text-5xl text-nowrap bg-clip-text text-transparent overflow-auto italic font-serif font-bold bg-gradient-to-r from-sky-200 to-sky-300">
             <RealTimeClock />
           </span>
@@ -34,19 +34,31 @@ export default async function page() {
           </span>
           <div>
             {!session && (
-              <div className="sm:text-2xl text-nowrap bg-clip-text text-transparent overflow-auto italic font-serif font-bold bg-gradient-to-r from-sky-200 to-sky-300">
-                <span>
-                  Would you like to{" "}
+              <div className="flex flex-row 
+                              sm:text-3xl lg:text-4xl
+                              bg-clip-text text-transparent
+                              italic font-serif font-bold
+                              bg-gradient-to-r from-sky-200 to-sky-300
+                              space-x-4 justify-center
+                              bg-clip-text text-transparent
+                              "
+              >
+                <div className="flex-1 text-right bg-[url('/waves.gif')] bg-cover bg-center p-5 rounded-2xl">
+                  {/* Would you like to{" "} */}
                   <Link href="/login" className="visit-links">
                     Login
                   </Link>{" "}
-                </span>
-                <span>
-                  or{" "}
+                </div>
+                <div className="p-5">
+                  {/* Would you like to{" "} */}
+                  Or
+                </div>
+                <div className="flex-1 text-left bg-[url('/waves.gif')] bg-cover bg-center p-5 rounded-2xl z-10">
+                  {/* or{" "} */}
                   <Link href="/register" className="visit-links">
                     Register
                   </Link>
-                </span>
+                </div>
               </div>
             )}
           </div>
