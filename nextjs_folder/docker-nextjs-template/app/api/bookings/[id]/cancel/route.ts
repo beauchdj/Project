fadfdf333286aa3b4d450c266f53e940bf1d015a
Jaclyn Sobolewski <124474391+JQuelll@users.noreply.github.com>
2025-12-01 20:54:22 -0600

    fixed so cancelled appt isn't deleted from bookings table

--- nextjs_folder/docker-nextjs-template/app/api/bookings/[id]/cancel/route.ts
+++ nextjs_folder/docker-nextjs-template/app/api/bookings/[id]/cancel/route.ts
@@ -1,9 +1,56 @@
 import { NextResponse } from "next/server";
 import { auth } from "@/auth";
-// import { cancelBookedAppt } from "@/app/lib/services/appointmentServices";
-import { pool } from "@/lib/db";
-import { Booking } from "@/app/lib/types/Booking";
+import {cancelBooking} from "../../../../lib/services/BookingService";
 
+type Params = {
+  params: { id: string };
+};
+
+export async function PATCH (request: Request, { params }: Params) {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { status } = await request.json();
+
+  if (status !== "Cancelled") {
+    return NextResponse.json(
+      { error: "Invalid booking status" },
+      {status: 400 }
+    );
+  }
+
+  try {
+   const booking = cancelBooking(params.id,session.user.id);
+
+  return NextResponse.json(
+    {message: "PATCH booking...", bookingId: params.id, status },
+    {status:200 }
+  );
+  } catch (error: any) {
+    console.error("Update booking error:", error);
+
+    if (error.code === "FORBIDDEN") {
+      return NextResponse.json({ error: error.message }, {status: 403 });
+    }
+
+    return NextResponse.json(
+      {message: "Server error" },
+      { status: 500 }
+    )
+  }
+}
+
+
+
+// import { cancelBookedAppt } from "@/app/lib/services/appointmentServices";
+//import { pool } from "@/lib/db";
+//import { BookingView } from "@/app/lib/types/BookingView";
+
+
+
+/*
 export async function PUT(
   request: Request,
   { params }: { params: Promise<{ id: string }> }
@@ -27,3 +74,4 @@ export async function PUT(
     return NextResponse.json({ error: "Server Error" }, { status: 500 });
   }
 }
+  */
