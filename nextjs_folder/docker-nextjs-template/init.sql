a5fcae560520b3e142161f27ac2af63f22858717
menaceNez <stankgav@gmail.com>
2025-12-03 12:28:25 -0600

    init.sql notificaions userid update and isactive addtion to appts_avail inserts

--- nextjs_folder/docker-nextjs-template/init.sql
+++ nextjs_folder/docker-nextjs-template/init.sql
@@ -68,11 +68,16 @@ CREATE TABLE public.appt_bookings (
 
 CREATE TABLE public.notifications (
     "apptid" uuid,
+    "userid" uuid,
     "status" character varying(64),
     "id" uuid DEFAULT gen_random_uuid() NOT NULL,
     CONSTRAINT "notifications_id" PRIMARY KEY ("id"),
+    CONSTRAINT "notifications_userid_fkey" FOREIGN KEY ("userid")
+    REFERENCES users(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
     CONSTRAINT "notifications_apptid_fkey" FOREIGN KEY ("apptid")
-    REFERENCES appt_bookings(id)
+    REFERENCES appts_avail(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );
@@ -90,13 +95,13 @@ INSERT INTO "users" ("id", "fullname", "hashpass", "street1", "street2", "city",
 ('82dabe93-46b5-4d2b-aafa-25343949d0fa',	'Gavin S',	'$2b$10$cwetqFvi49nWQ.eSVr3Aq.lqtOBvCs4f4G3p1yxFYqsLr86NsR9/C',	'986',	'986',	'mad',	'wi',	'986',	'986',	'986',	'user',	'medical',	'1',	'1',	'1',	'986',	'Super Sweets'),
 ('6d18a65f-570a-4a74-8d48-43228518a53a',	'Sophia Coon',	'$2b$10$8R42WNqjd/zJtYKba/kYCeYRf8jw.TsARN3RxSpLwH3y3bHBOeWme',	'123 st',	'986',	'mad',	'wi',	'54664',	'123-123-1234',	'sophia@gmail.com',	'sophia',	NULL,	'0',	'0', '1', NULL, NULL);
 
-INSERT INTO "appts_avail" ("id", "spid", "starttime", "endtime", "service") VALUES
-('6226098d-05bb-42ed-a9f0-30e7801a6a6e',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-22 15:09:00',	'2025-11-22 15:56:00',	'Physical Therapy'),
-('fd756ad3-d8fb-45d3-b101-1ce91f06b8a0',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-09 23:22:00',	'2025-11-09 23:45:00',	'Back replacement'),
-('a0fee661-e04e-4364-8f18-dcd9b6873e06',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-28 02:44:00',	'2025-11-28 04:03:00',	'Rice eating'),
-('c4a60331-6e79-4031-ad27-d948c5b2b915',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 03:00:00',	'2025-11-20 03:45:00',	'Liver transplant'),
-('f12e3ccb-b7c6-4f17-874d-717c71bae146',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-27 08:00:00',	'2025-11-27 08:45:00',	'Femur Replacement'),
-('612e8671-8245-42a1-b034-ae3ba7f89bae',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 08:00:00',	'2025-11-20 08:45:00',	'test'),
-('bd02d2df-cc8e-4e10-b087-f1ef01bd93bf',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 08:50:00',	'2025-11-20 08:55:00',	'testw2'),
-('4ba247eb-5e4a-4904-b06b-0b4c4660dffc',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-12 04:00:00',	'2025-11-12 04:45:00',	'Heart Transplant'),
-('793e4247-5846-4d6b-89c0-2a818ec84ee4',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-11 16:00:00',	'2025-11-11 16:45:00',	'Liver Transplant');
+INSERT INTO "appts_avail" ("id", "spid", "starttime", "endtime", "isactive", "service") VALUES
+('6226098d-05bb-42ed-a9f0-30e7801a6a6e',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-22 15:09:00',	'2025-11-22 15:56:00',  '0',  'Physical Therapy'),
+('fd756ad3-d8fb-45d3-b101-1ce91f06b8a0',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-09 23:22:00',	'2025-11-09 23:45:00',	'0',  'Back replacement'),
+('a0fee661-e04e-4364-8f18-dcd9b6873e06',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-28 02:44:00',	'2025-11-28 04:03:00',	'0',  'Rice eating'),
+('c4a60331-6e79-4031-ad27-d948c5b2b915',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 03:00:00',	'2025-11-20 03:45:00',	'0',  'Liver transplant'),
+('f12e3ccb-b7c6-4f17-874d-717c71bae146',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-27 08:00:00',	'2025-11-27 08:45:00',	'0',  'Femur Replacement'),
+('612e8671-8245-42a1-b034-ae3ba7f89bae',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 08:00:00',	'2025-11-20 08:45:00',	'0',  'test'),
+('bd02d2df-cc8e-4e10-b087-f1ef01bd93bf',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 08:50:00',	'2025-11-20 08:55:00',	'0',  'testw2'),
+('4ba247eb-5e4a-4904-b06b-0b4c4660dffc',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-12 04:00:00',	'2025-11-12 04:45:00',	'0',  'Heart Transplant'),
+('793e4247-5846-4d6b-89c0-2a818ec84ee4',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-11 16:00:00',	'2025-11-11 16:45:00',	'0',  'Liver Transplant');
