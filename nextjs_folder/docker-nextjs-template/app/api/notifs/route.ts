2b47302b48703f2a5122cabc7a1260a2c901506b
Jaclyn Sobolewski <124474391+JQuelll@users.noreply.github.com>
2025-12-06 16:42:31 -0600

    notifs route as alternative to notifications

---
+++ nextjs_folder/docker-nextjs-template/app/api/notifs/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+import { getNotificationsForUser, updateNotificationStatus} from "../../lib/services/notificationService";
+
+export async function GET(request: Request) {
+    const session = await auth();
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    try {
+        const notifications = await getNotificationsForUser(session.user.id);
+        return NextResponse.json(
+            { notifications },
+            {status: 200 }
+        );
+    } catch (error: any) {
+        console.error("Get Notifications error:", error);
+        return NextResponse.json(
+            { message: "Server Error" },
+            {status: 500}
+        );
+    }
+}
+
+export async function PATCH (request: Request) {
+    const session = await auth();
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const {notifId, isActive, isNew } = await request.json();
+
+    if (!notifId) {
+        return NextResponse.json(
+            { error: "notifId required" },
+            { status: 400 }
+        );
+    }
+
+    await updateNotificationStatus(notifId, isActive, isNew);
+        return NextResponse.json(
+                { message: "Notification updated successfully" },
+                { status: 200 }
+            );
+  
+}
