9a65023acf1c069c736c3a1746fb339ca34bcbc4
Jaclyn Sobolewski <124474391+JQuelll@users.noreply.github.com>
2025-12-08 14:51:39 -0600

    cleaned up demo data

--- nextjs_folder/docker-nextjs-template/init.sql
+++ nextjs_folder/docker-nextjs-template/init.sql
@@ -121,18 +121,9 @@ INSERT INTO "users" ("id", "fullname", "hashpass", "street1", "street2", "city",
 ('82dabe93-46b5-4d2b-aafa-25343949d0fa',	'Gavin S',	'$2b$10$cwetqFvi49nWQ.eSVr3Aq.lqtOBvCs4f4G3p1yxFYqsLr86NsR9/C',	'986',	'986',	'mad',	'wi',	'986',	'986',	'986',	'user',	'medical',	'1',	'1',	'1',	'986',	'Super Sweets','1'),
 ('6d18a65f-570a-4a74-8d48-43228518a53a',	'Sophia Coon',	'$2b$10$8R42WNqjd/zJtYKba/kYCeYRf8jw.TsARN3RxSpLwH3y3bHBOeWme',	'123 st',	'986',	'mad',	'wi',	'54664',	'123-123-1234',	'sophia@gmail.com',	'sophia',	NULL,	'0',	'0', '1', NULL, NULL,'1'),
 ('f780da56-3d73-4ec4-9114-9a9374abc3a2',	'admin',	'$2b$10$Wu.53SPnkg8Zxz4TgEzNYOB0A8GRtHe6uZEBmkvM.m9aG9f6rjlA.',	'str',	'',	'city',	'wi',	'12345',	'1234567890',	'admin@gmail.com',	'admin',	'beauty',	'1',	'1',	'1',	'quals',	'Admin','1'),
-('09a02678-d8a8-4520-a8fd-d1ed8a79a02b',  'Tom Klint', '$2b$10$7PMQqdoK0rP1T5fG5wXHMOz6WICe7hpWFEVSpjCK9JsFMcklgUk4a', '123','','Madison','WI','53703','1234567890','tom@gmail.com','tom','fitness','0','1','1','yoga certification','Yoga with Tom', '1');
+('09a02678-d8a8-4520-a8fd-d1ed8a79a02b',  'Tom Klint', '$2b$10$7PMQqdoK0rP1T5fG5wXHMOz6WICe7hpWFEVSpjCK9JsFMcklgUk4a', '123','','Madison','WI','53703','1234567890','tom@gmail.com','tom','fitness','0','1','1','yoga certification','Tom Klint Fitness', '1');
 
 INSERT INTO "appts_avail" ("id", "spid", "starttime", "endtime", "isactive", "service") VALUES
-('6226098d-05bb-42ed-a9f0-30e7801a6a6e',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-22 15:09:00',	'2025-11-22 15:56:00',  '0',  'Physical Therapy'),
-('fd756ad3-d8fb-45d3-b101-1ce91f06b8a0',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-09 23:22:00',	'2025-11-09 23:45:00',	'0',  'Back replacement'),
-('a0fee661-e04e-4364-8f18-dcd9b6873e06',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-28 02:44:00',	'2025-11-28 04:03:00',	'0',  'Rice eating'),
-('c4a60331-6e79-4031-ad27-d948c5b2b915',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 03:00:00',	'2025-11-20 03:45:00',	'0',  'Liver transplant'),
-('f12e3ccb-b7c6-4f17-874d-717c71bae146',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-27 08:00:00',	'2025-11-27 08:45:00',	'0',  'Femur Replacement'),
-('612e8671-8245-42a1-b034-ae3ba7f89bae',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 08:00:00',	'2025-11-20 08:45:00',	'0',  'test'),
-('bd02d2df-cc8e-4e10-b087-f1ef01bd93bf',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-20 08:50:00',	'2025-11-20 08:55:00',	'0',  'testw2'),
-('4ba247eb-5e4a-4904-b06b-0b4c4660dffc',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-12 04:00:00',	'2025-11-12 04:45:00',	'0',  'Heart Transplant'),
-('793e4247-5846-4d6b-89c0-2a818ec84ee4',	'82dabe93-46b5-4d2b-aafa-25343949d0fa',	'2025-11-11 16:00:00',	'2025-11-11 16:45:00',	'0',  'Liver Transplant'),
 ('c6497be7-0dcc-4c9f-a5cb-f7317b487a47',  'bb6ac427-5643-40ce-ab2b-e00c380d195b', '2025-12-16 15:30:00',  '2025-12-16 16:30:00',  '1',  'Perm'),
 ('99fc2ba2-028f-46e5-bbfd-c2506369417d',  'bb6ac427-5643-40ce-ab2b-e00c380d195b', '2025-12-16 16:30:00',  '2025-12-16 17:30:00',  '1',  'Relaxer'),
 ('7852f713-b1c7-4249-9333-8f9893dbd7fa',  '09a02678-d8a8-4520-a8fd-d1ed8a79a02b', '2025-12-16 15:30:00',  '2025-12-16 16:30:00',  '1',  'Yoga'),
